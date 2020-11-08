ac4081db867f1268285499f3fecce1b0f5f79f1b
hirokatsuiino <letterbox6212>
2020-11-08 14:22:56 +0900

    [ADD]article add culumm name

--- myblog/db/schema.rb
+++ myblog/db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_07_232401) do
+ActiveRecord::Schema.define(version: 2020_11_08_051609) do
 
   create_table "articles", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
   end
 
 end
