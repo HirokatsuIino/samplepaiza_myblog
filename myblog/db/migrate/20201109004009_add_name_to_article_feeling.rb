46d60fe3498e916c4d847919628b8a16c74d9193
hirokatsuiino <letterbox6212>
2020-11-09 09:45:55 +0900

    [ADD]article culumm

---
+++ myblog/db/migrate/20201109004009_add_name_to_article_feeling.rb
@@ -0,0 +1,5 @@
+class AddNameToArticleFeeling < ActiveRecord::Migration[6.0]
+  def change
+    add_column :articles, :feeling, :string
+  end
+end
--- myblog/db/schema.rb
+++ myblog/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_09_003104) do
+ActiveRecord::Schema.define(version: 2020_11_09_004009) do
 
   create_table "articles", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
+    t.string "feeling"
   end
 
   create_table "diaries", force: :cascade do |t|
