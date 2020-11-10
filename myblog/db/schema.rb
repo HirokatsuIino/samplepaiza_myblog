e812c49afc36eadbbc3888d85a069349751d65e8
hirokatsuiino <letterbox6212>
2020-11-10 14:37:44 +0900

    [ADD]user_id

--- myblog/db/schema.rb
+++ myblog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_10_025238) do
+ActiveRecord::Schema.define(version: 2020_11_10_051836) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "content"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_11_10_025238) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.string "feeling"
+    t.integer "user_id"
   end
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
