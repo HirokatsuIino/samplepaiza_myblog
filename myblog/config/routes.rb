72e4e255889abfc2ee15524d729422605171e654
hirokatsuiino <letterbox6212>
2020-11-10 11:32:42 +0900

    [ADD]Welcome top page

--- myblog/config/routes.rb
+++ myblog/config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
   resources :categories
   resources :shops
   resources :diaries
   resources :articles
 
-  root "shops#index"
+  root "welcome#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
