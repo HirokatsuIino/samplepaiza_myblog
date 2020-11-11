ba727c13ae922ca929009dc5dae0a8d5b345066e
hirokatsuiino <letterbox6212>
2020-11-11 09:27:13 +0900

    [ADD]protected -> protected_methods へ変更

--- myblog/app/controllers/application_controller.rb
+++ myblog/app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
   end
 end
 
-protected
+protected_methods
 
 def configure_permitted_parameters
   devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
