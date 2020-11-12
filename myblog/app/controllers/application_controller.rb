65379b67812d1f5d3a8d8be2345da9f553bbf375
hirokatsuiino <letterbox6212>
2020-11-12 18:18:51 +0900

    [ADD]認証機能追加

--- myblog/app/controllers/application_controller.rb
+++ myblog/app/controllers/application_controller.rb
@@ -10,11 +10,13 @@ class ApplicationController < ActionController::Base
   def default_url_options(options = {})
     { locale: I18n.locale }.merge options
   end
+
+  protected_methods
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
 end
 
-protected_methods
-
-def configure_permitted_parameters
-  devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-  devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-end
