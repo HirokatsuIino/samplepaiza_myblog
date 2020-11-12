9e0d0874f740d249bae8522ab5c86c2509380e47
hirokatsuiino <letterbox6212>
2020-11-12 18:25:09 +0900

    [ADD]webpack vueファイル郡

---
+++ myblog/app/javascript/app.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <p>{{ message }}</p>
+    <p>{{ message2 }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      message: "Hello Vue!",
+      message2: "Hello Vue!a"
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
---
+++ myblog/app/javascript/app2.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <table>
+      <tbody>
+      <tr>
+        <th>ID</th>
+        <th>name</th>
+        <th>content</th>
+        <th>created_at</th>
+      </tr>
+      <tr v-for="e in employees" :key="e.id">
+        <td>{{ e.id }}</td>
+        <td>{{ e.name }}</td>
+        <td>{{ e.content }}</td>
+        <td>{{ e.created_at }}</td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+    import axios from 'axios';
+
+    export default {
+        data: function () {
+            return {
+                employees: []
+            }
+        },
+        mounted () {
+            axios
+                .get('/api/v1/articles.json')
+                .then(response => (this.employees = response.data))
+        }
+    }
+</script>
+
+<style scoped>
+  p {
+    font-size: 2em;
+    text-align: center;
+  }
+</style>
