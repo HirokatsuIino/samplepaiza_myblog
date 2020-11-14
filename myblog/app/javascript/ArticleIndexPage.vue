0de8627b1f7ede0bbd862340f01d5541de65cfe6
hirokatsuiino <letterbox6212>
2020-11-14 13:51:09 +0900

    [ADD]]artice一覧をvue化

---
+++ myblog/app/javascript/ArticleIndexPage.vue
@@ -0,0 +1,56 @@
+<template>
+    <div id="articles">
+        <table>
+            <tbody>
+            <tr>
+                <th>ID</th>
+                <th>name</th>
+                <th>content</th>
+                <th>created_at</th>
+            </tr>
+            <tr v-for="e in employees" :key="e.id">
+                <td>{{ e.id }}</td>
+                <td>{{ e.name }}</td>
+                <td>{{ e.content }}</td>
+                <td>{{ e.created_at }}</td>
+                <td><router-link :to="`/articles/${ e.id }`">詳細</router-link></td>
+                <!--<td><router-link :to="`/articles/${ e.id }`">詳細</router-link></td>-->
+                <!--<td><router-link :to="{name: 'ArticleDetailPage', params: {id: '1' }}" class="btn btn-info">詳細</router-link></td>-->
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios';
+
+    export default {
+        name: "ArticleIndexPage.vue",
+        data: function () {
+            return {
+                employees: []
+            }
+        },
+        mounted () {
+            axios
+                .get('/api/v1/articles.json')
+                .then(response => (this.employees = response.data))
+        }
+
+    }
+</script>
+
+<style scoped>
+    p {
+        font-size: 2em;
+        text-align: center;
+    }
+
+</style>
+
+
+
+
+
+
--- myblog/app/javascript/app2.vue
+++ myblog/app/javascript/app2.vue
@@ -1,44 +1,41 @@
 <template>
-  <div id="app">
-    <table>
-      <tbody>
-      <tr>
-        <th>ID</th>
-        <th>name</th>
-        <th>content</th>
-        <th>created_at</th>
-      </tr>
-      <tr v-for="e in employees" :key="e.id">
-        <td>{{ e.id }}</td>
-        <td>{{ e.name }}</td>
-        <td>{{ e.content }}</td>
-        <td>{{ e.created_at }}</td>
-      </tr>
-      </tbody>
-    </table>
-  </div>
+    <div>
+        <router-view></router-view>
+    </div>
 </template>
 
 <script>
-    import axios from 'axios';
+    import Vue from 'vue'
+    import VueRouter from 'vue-router'
+
+    import ArticleIndexPage from 'ArticleIndexPage.vue'
+    import ArticleDetailPage from 'ArticleDetailPage.vue'
+    import NotFound from 'NotFound.vue'
+
+    const router = new VueRouter({
+        mode: 'history',
+        routes: [
+            { path: '*',　//ここから3行追記
+                name: NotFound,
+                component: NotFound },
+            { path: '/articles',
+                name: ArticleIndexPage,
+                component: ArticleIndexPage  },
+            { path: '/articles/:id',  // :id は数値のみに制限する
+            // { path: '/articles/:id(\\d+)',  // :id は数値のみに制限する
+                name: ArticleDetailPage,
+                component: ArticleDetailPage  },
+
+        ]
+    })
+
+    // ref. https://jp.vuejs.org/v2/guide/plugins.html#%E3%83%97%E3%83%A9%E3%82%B0%E3%82%A4%E3%83%B3%E3%81%AE%E4%BD%BF%E7%94%A8
+    Vue.use(VueRouter)
 
     export default {
-        data: function () {
-            return {
-                employees: []
-            }
-        },
-        mounted () {
-            axios
-                .get('/api/v1/articles.json')
-                .then(response => (this.employees = response.data))
-        }
+        router
     }
 </script>
 
 <style scoped>
-  p {
-    font-size: 2em;
-    text-align: center;
-  }
 </style>
