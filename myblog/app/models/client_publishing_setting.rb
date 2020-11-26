81fc8251584ecfc4d11d49933475be90d7466882
hirokatsuiino <letterbox6212>
2020-11-26 15:57:30 +0900

    [ADD]リレーション

--- myblog/app/models/client_publishing_setting.rb
+++ myblog/app/models/client_publishing_setting.rb
@@ -1,3 +1,10 @@
 class ClientPublishingSetting < ApplicationRecord
-  # belongs_to :client, foreign_key: 'id'
+  belongs_to :client
+  has_many :publishing_terms, through: :client
 end
+
+
+# ClientPublishingSetting.limit(1).each do |cp|
+#   p cp.client.admin_comment
+#   p cp.client.client_unit_price.unit_price_id
+# end
