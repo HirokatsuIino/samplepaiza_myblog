9af867d91532228633103f0df1fa0a4927cd0c22
hirokatsuiino <letterbox6212>
2020-12-07 20:15:59 +0900

    [MODEF]投入データ調整

--- myblog/db/fixtures/development/client.rb
+++ myblog/db/fixtures/development/client.rb
@@ -3,21 +3,21 @@ require 'csv'
 csv = CSV.read('db/fixtures/development/client.csv', encoding: 'Shift_JIS:UTF-8')
 csv.each do |line|
   # id = line[0]
-  client_type_id = line[1]
+  # client_type_id = line[1]
   name = line[2]
   admin_comment = line[3]
   area_important = line[4]
-  nav_id = line[5]
+  # nav_id = line[5]
 
 
 
   Client.seed_once(:id) do |s|
     # s.id = id
-    s.client_type_id = client_type_id
+    # s.client_type_id = client_type_id
     s.name = name
     s.admin_comment = admin_comment
     s.area_important = area_important
-    s.nav_id = nav_id
+    # s.nav_id = nav_id
   end
 end
 
